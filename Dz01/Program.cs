322e85bc4a34a56831d6f011bf849fe449a9e25b
NEVA <neva.vka@yandex.ru>
2022-10-13 22:55:28 +0300

    Задача 25 степень

--- Dz01/Program.cs
+++ Dz01/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.Clear();
+
+double Pow (int a, int b)
+{
+   double res =  Math.Pow(a,b);
+   return res;
+}
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( Pow(a,b));
+
+
+
+
+
+
