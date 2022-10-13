04fa45a57231b30b675e25f992094fba631d2823
NEVA <neva.vka@yandex.ru>
2022-10-13 23:28:03 +0300

    Задача 27, сумма цифр

---
+++ Dz02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int c = 0;
+int sum = 0;
+int f = 0;
+while(c < a)
+{
+    f = a%10;
+    sum = f + sum;
+    a = a/10;
+}
+
+Console.WriteLine($"Сумма чисел = {sum}");
