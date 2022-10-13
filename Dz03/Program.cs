4a41eaa419d000b61631e7783a60dbd3fe176833
NEVA <neva.vka@yandex.ru>
2022-10-14 01:12:30 +0300

    Задача 29 массив

---
+++ Dz03/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+
+int [] arr;
+arr = new int[8];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    int num = new Random().Next(1,9);
+    arr[i] = num;
+}    
+    
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+
+
+
+
